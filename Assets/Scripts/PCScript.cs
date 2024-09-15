0bc02077c4fc0dedc042e25f61676f57f401da3c
niels <nielslouwes45@gmail.com>
2024-09-15 17:39:00 +0200

    add email deletion conosole prompt

--- Assets/Scripts/PCScript.cs
+++ Assets/Scripts/PCScript.cs
@@ -1,28 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PCScript : MonoBehaviour
 {
+    public Transform player;
+    public float detectionDistance = 2f;
+    public TextMeshProUGUI promptText;
+    private bool canInteract = false;
+    private bool emailDeleted = false;
 
-   public Transform player;
-   public float detectionDistance = 2f;
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Hide the text prompt initially
+        promptText.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // player position , position of our game object
+        // Calculate the distance between the player and the PC
         float distance = Vector3.Distance(player.position, transform.position);
         
-        if (distance < detectionDistance) {
-             Debug.Log("Player is within range!");
-        } else {
-            Debug.Log("Player is out of range!");
+        if (distance < detectionDistance && !emailDeleted)
+        {
+            // If the player is close enough and the email isn't deleted, show the prompt
+            promptText.gameObject.SetActive(true);
+            canInteract = true;
         }
+        else
+        {
+            // Otherwise, hide the prompt and prevent interaction
+            promptText.gameObject.SetActive(false);
+            canInteract = false;
+        }
+
+        // Only allow interaction if the player can interact and hasn't deleted the email
+        if (canInteract && Input.GetKeyDown(KeyCode.Space) && !emailDeleted)
+        {
+            DeleteEmail();
+        }
+    }
+
+    void DeleteEmail()
+    {
+        Debug.Log("Email Deleted!");
+        promptText.gameObject.SetActive(false);
+        emailDeleted = true; // Mark email as deleted
     }
 }
